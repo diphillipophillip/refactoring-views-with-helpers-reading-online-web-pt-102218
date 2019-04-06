1b2f03435cca347832644761d671dba504fb7139
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-04-06 20:13:11 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params[:id])
+	  @post.update(params)
 	  redirect_to post_path(@post)
 	end
 
